1cdd04926d94541fc0e080fd57f4755dee7a8d72
DaiM24 <dayml.0916@gmail.com>
2022-12-12 12:35:42 -0300

    :construction: Feat: Add changeMap funtion, buttonClosed and "G" icon on tab

--- js/map.js
+++ js/map.js
@@ -1,25 +1,22 @@
 // En coordinates guardamos las coordenadas donde se lugar donde se centra el mapa
-let coordinates = [25.2807, 51.5217];
+let coordinates = [25.2841478, 51.4419568];
 // En la variable map enviamos el centrod de mapa y el zoom inicial (12)
-let map = L.map('map').setView(coordinates, 12);
+let map = L.map('map').setView(coordinates, 11);
 // Creamos el mapa
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
-
-// Icono
-let position = L.icon({
+//Icono
+let icon = L.icon({
     iconUrl: "../assets/marker.png",
     iconSize: [30, 45],
     iconAnchor: [20, 44],
     popupAnchor: [18, -41]
 });
-
-let marker = L.marker(coordinates, {icon: position}).addTo(map);
+let marker = L.marker(coordinates, {icon: icon}).addTo(map);
 // popUp
-marker.bindPopup("<b>Lo logramos!!!</b>").openPopup();
-// var marker = L.marker(coordinates).addTo(map);
+marker.bindPopup('<b>Lo logramos!!!</b>').openPopup();
 
 const objetoMapa = [
     {
@@ -54,9 +51,30 @@ const objetoMapa = [
     },
 ];
 
+const select = document.querySelector('select');
+      //select.onchange = changeMap
+// Opcion 1
+let jugadores = objetoMapa.map((objeto) =>`<option>${objeto.jugador}</option>`)
+let jugadoresOptions = jugadores.join().replace(',', '')
+select.innerHTML = jugadoresOptions
+
+// Opcion 2
+// objetoMapa.map((objeto) =>{
+//     let option = document.createElement('option')
+//     option.innerHTML = objeto.jugador;
+//     select.appendChild(option)
+// })
+
 function changeMap() {
     const objeto = objetoMapa.find((item) => item.jugador === select.value);
     map.setView(new L.LatLng(...objeto.coordenada), 11);
     marker = L.marker(objeto.coordenada, { icon }).addTo(map);
     marker.bindPopup(`<b>${objeto.jugador}</b><br>${objeto.ciudad}`).openPopup();
 }
+
+let buttonClosed = document.getElementById('cerrarSesion')
+
+buttonClosed.addEventListener('click', ()=>{
+    localStorage.removeItem("logged", "true");
+    window.location.href = '../index.html';
+})
